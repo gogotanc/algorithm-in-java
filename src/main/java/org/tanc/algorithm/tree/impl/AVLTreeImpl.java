c4a021edfaefd7481bf968473769796fe9ae95d6
gogotanc <gogotanc@163.com>
2017-02-10 20:56:14 +0800

    avl tree

---
+++ src/main/java/org/tanc/algorithm/tree/impl/AVLTreeImpl.java
@@ -0,0 +1,53 @@
+package org.tanc.algorithm.tree.impl;
+
+import org.tanc.algorithm.tree.BinaryTree;
+
+/**
+ * AVL (Adelson-Belskii Landis) 树
+ * 带有平衡条件的二叉查找树
+ * Created by tanc on 2017/2/10.
+ */
+public class AVLTreeImpl<E> implements BinaryTree<E> {
+
+    // todo avl tree need to being finished
+
+    @Override
+    public void makeEmpty() {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(E element) {
+        return false;
+    }
+
+    @Override
+    public E findMin() {
+        return null;
+    }
+
+    @Override
+    public E findMax() {
+        return null;
+    }
+
+    @Override
+    public void insert(E element) {
+
+    }
+
+    @Override
+    public void remove(E element) {
+
+    }
+
+    @Override
+    public void printTree() {
+
+    }
+}
