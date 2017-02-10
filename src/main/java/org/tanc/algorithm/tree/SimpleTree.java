64050b49e0d34976047d621f5b1a5679bab926e2
gogotanc <gogotanc@163.com>
2017-02-10 15:46:16 +0800

    normal tree

---
+++ src/main/java/org/tanc/algorithm/tree/SimpleTree.java
@@ -0,0 +1,18 @@
+package org.tanc.algorithm.tree;
+
+/**
+ * 常规树的实现
+ * Created by tanc on 2017/2/10.
+ */
+public class SimpleTree {
+
+    private TreeNode root;
+
+    public TreeNode getRoot() {
+        return root;
+    }
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
+}
